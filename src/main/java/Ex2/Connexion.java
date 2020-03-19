3a33fd90763c6805108af16202bbfca10242ebf1
David Binaud <david.binaud@etu.umontpellier.fr>
2020-03-19 17:43:49 +0100

    Exercice 2 Q2

---
+++ src/main/java/Ex2/Connexion.java
@@ -0,0 +1,41 @@
+package Ex2;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Connexion implements Runnable {
+
+    private Socket soc;
+
+    public Connexion(Socket soc) {
+        this.soc = soc;
+    }
+
+    public void run() {
+
+        try {
+            BufferedReader ins = new BufferedReader(
+                    new InputStreamReader(soc.getInputStream()));
+            PrintWriter outs = new PrintWriter(new BufferedWriter(
+                    new OutputStreamWriter(soc.getOutputStream())), true);
+
+            while(true) {
+                String strIN = ins.readLine();
+                if(strIN.equals("stop")) break;
+
+                System.out.println(strIN);
+                String strOUT = "Reçu:" + strIN;
+                outs.println(strOUT);
+            }
+            System.out.println("stop");
+            outs.println("stop");
+
+            ins.close();
+            outs.close();
+            soc.close();
+        }catch (java.io.IOException e){
+            e.printStackTrace();
+        }
+    }
+}
