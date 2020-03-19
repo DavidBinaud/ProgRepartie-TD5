4ab33352a9d8415cf982abdc94310e5eb66fcd96
David Binaud <david.binaud@etu.umontpellier.fr>
2020-03-19 16:36:53 +0100

    Exercice 1 Q2

---
+++ src/main/java/Serveur.java
@@ -0,0 +1,32 @@
+import java.net.*;
+import java.io.*;
+
+public class Serveur {
+    public static void main(String[] args) throws Exception {
+
+        ServerSocket s = new ServerSocket(6020);
+        System.out.println("START");
+        System.out.println(s.getInetAddress());
+
+        Socket soc = s.accept();
+        System.out.println("Client Connecté");
+        BufferedReader ins = new BufferedReader(
+                new InputStreamReader(soc.getInputStream()));
+        PrintWriter outs = new PrintWriter(new BufferedWriter(
+                new OutputStreamWriter(soc.getOutputStream())), true);
+
+        // insertion de la boucle du serveur ici
+        while(true) {
+            String strIN = ins.readLine();
+            if(strIN.equals("END")) break;
+
+            System.out.println(strIN);
+            String strOUT = "Reçu:" + strIN;
+            outs.println(strOUT);
+        }
+
+        ins.close();
+        outs.close();
+        soc.close();
+    }
+}
